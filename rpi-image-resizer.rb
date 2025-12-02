ebd575e5e0b4fc42856f9e8ee7eb35397686d30a
Andreas Heissenberger <andreas@heissenberger.at>
2025-12-02 05:07:14 +0100

    chore(release): v0.0.23

--- rpi-image-resizer.rb
+++ rpi-image-resizer.rb
@@ -1,13 +1,13 @@
 class RpiImageResizer < Formula
   desc "Raspberry Pi image resize and partition adjuster (CLI)"
   homepage "https://github.com/aheissenberger/raspberry-image-resizer-docker"
-  version "0.0.22"
+  version "0.0.23"
 
   if Hardware::CPU.arm?
-    url "https://github.com/aheissenberger/raspberry-image-resizer-docker/releases/download/v0.0.22/rpi-tool-darwin-arm64.tar.gz"
-    sha256 "2aa6ff708ce340137814a6713bb65dc2f33d9105d95c71c8c8ff4c0b623f8c41"
+    url "https://github.com/aheissenberger/raspberry-image-resizer-docker/releases/download/v0.0.23/rpi-tool-darwin-arm64.tar.gz"
+    sha256 "24e2772e16a47d7785c164465a6a5c392c0fa34ef7d82af18e35a452432cfe26"
   else
-    url "https://github.com/aheissenberger/raspberry-image-resizer-docker/releases/download/v0.0.22/rpi-tool-darwin-amd64.tar.gz"
+    url "https://github.com/aheissenberger/raspberry-image-resizer-docker/releases/download/v0.0.23/rpi-tool-darwin-amd64.tar.gz"
     sha256 "0000000000000000000000000000000000000000000000000000000000000000"
   end
 
